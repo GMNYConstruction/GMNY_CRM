7255bdbad0855ac97425e6d11a8947a34a61ff78
VensanDrot <vensandrot@gmail.com>
2024-03-27 15:08:05 -0400

    Fix class issues, remove console.logs

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -22,18 +22,18 @@ export const TextArea: FC<IProps> = ({
   const ref = useRef<HTMLTextAreaElement>(null);
 
   if (resize && ref.current) {
-    ref.current.style.width = ref.current.clientWidth / 1.9 + "px";
+    ref.current.style.width = "360px";
     ref.current.style.height = ref.current.scrollHeight + "px";
     ref.current.style.maxHeight = ref.current.scrollHeight + "px";
-  } else if (ref.current) {
-    ref.current.style.width = "100%";
-    ref.current.style.height = "auto";
-    ref.current.style.maxHeight = "auto";
+  } else if (ref.current && !resize) {
+    ref.current.style.width = "";
+    ref.current.style.height = "";
+    ref.current.style.maxHeight = "";
   }
 
   return (
     <textarea
-      className={`w-full min-h-[200px]  h-0 px-4 py-3 bg-white rounded-lg shadow border border-neutral-200 flex-col ${properties}`}
+      className={`w-full min-h-[200px] max-h-[270px] h-0 px-4 py-3 bg-white rounded-lg shadow border border-neutral-200 flex-col ${properties}`}
       name={id}
       id={id}
       ref={ref}
