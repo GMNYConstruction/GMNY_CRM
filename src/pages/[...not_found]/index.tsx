0acd1bb6e052fb93c661ad79e1eca02556feda6b
VensanDrot <vensandrot@gmail.com>
2024-07-23 16:01:20 -0400

    lastModified with time zone and date

--- src/pages/[...not_found]/index.tsx
+++ src/pages/[...not_found]/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../../img/logo.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const NotFoundCatchAll = () => {
   return (
@@ -8,6 +9,9 @@ const NotFoundCatchAll = () => {
       <Image className="h-[160px] w-[160px] rounded-lg self-center" src={logo} alt="" />
       <h1 className="text-5xl font-medium self-center">Working on it</h1>
       <h2 className="text-3xl font-medium self-center text-neutral-500">Not Found</h2>
+      <Link href="/" className="text-3xl font-medium self-center text-blue-600">
+        Home
+      </Link>
     </div>
   );
 };
