24c4499a75821449e6506601919c5ff4af579922
VensanDrot <vensandrot@gmail.com>
2024-07-25 12:11:33 -0400

    connected all webforms to pages, added forms to sidemenu

---
+++ src/pages/forms/spanish/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="w-full h-full">
+      <iframe
+        src="https://na4.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhAaDAPynCP2_zhzZze9l-cxxEFEg8nCq7p0pXbeLYpyCGBl4CTZhHYizJN33k7mDHA*&hosted=false"
+        className="min-h-[460px] h-full"
+        width="100%"
+        height="100%"
+        loading="eager"
+      ></iframe>
+    </div>
+  );
+};
+
+export default index;
