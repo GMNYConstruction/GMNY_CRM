4dd4b294a71e7483c3d94d69c2faaf9d28e73c12
VensanDrot <vensandrot@gmail.com>
2024-07-25 12:38:33 -0400

    Move to the same form diffrent language display link on top, center the form make it smaller

--- src/pages/forms/englishfull/index.tsx
+++ src/pages/forms/englishfull/index.tsx
@@ -1,13 +1,18 @@
+import Link from "next/link";
 import React from "react";
+import icback from "../../../img/ic_back.svg";
+import Image from "next/image";
 
 const index = () => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full flex flex-col gap-2">
+      <Link className="hidden xs-max:flex h-fit p-2 items-center gap-2" href={"/forms/spanishfull"}>
+        <Image src={icback} alt="back" />
+        Versión en español
+      </Link>
       <iframe
         src="https://na4.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhA1nYYRJgfM6dN4D8MJmNJeM1enpC_FHi5b0IpOdZUAC2uQrk_s5AJ5bpS179qDNjg*&hosted=false"
-        className="min-h-[460px] h-full"
-        width="100%"
-        height="100%"
+        className="min-h-[460px] xs-max:!h-[90%] w-full h-full self-center"
         loading="eager"
       ></iframe>
     </div>
