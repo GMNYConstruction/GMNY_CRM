286894c87afa199bb6222dcb3a3eccf6ce30cd64
VensanDrot <vensandrot@gmail.com>
2024-08-22 15:39:18 -0400

    fix the issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 "use client";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React from "react";
 
 export default function Home() {
-  const navigate = useRouter();
-
-  navigate.push("/caselist");
   return (
     <div className="w-full h-full relative pr-px items-center bg-white flex flex-col justify-center">
       <h1> New Pages Are Comming Soon!!</h1>
